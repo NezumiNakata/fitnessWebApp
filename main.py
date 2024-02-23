11f704c840d628f56b2fae2158fff9570f4adbb6
dgross16 <danielgross13382@gmail.com>
2024-02-23 18:43:47 -0500

    test

--- main.py
+++ main.py
@@ -1,22 +1,28 @@
-from flask import Flask, redirect, url_for, render_template
+from flask import Flask, redirect, url_for, render_template, request
+from markupsafe import escape
 
 app = Flask(__name__)
+
 
 @app.route("/")
 def home():
     return "Hello! This the main page <h1>HELLO<h/1>"
 
+
 @app.route("/<name>")
 def user(name):
-    return f"Hello {name}!"
+    return f"Hello {escape(name)}!"
+
 
 @app.route("/admin")
 def admin():
     return redirect(url_for("user", name="Admin"))
 
-@app.route("/lice")
-def lice():
-    return render_template("lice.html")
+
+@app.route("/index")
+def index():
+    return render_template("./templates/index.html")
+
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
