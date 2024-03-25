4e0e1504128a35c199c82305307f5715aa5b6071
dgross16 <danielgross13382@gmail.com>
2024-03-25 19:51:47 -0400

    post request & other QoL things

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
 from flask import Flask, redirect, url_for, render_template, request
 from markupsafe import escape
 
+
 app = Flask(__name__)
+
+users = {
+        'admin': '12345',
+        'drewPeacock': '12345'
+        }
 
 
 @app.route("/")
@@ -14,6 +20,25 @@ def user(name):
     return f"Hello {escape(name)}!"
 
 
+@app.route("/login_page")
+def login_page():
+    return render_template("login.html")
+
+
+@app.route("/login", methods=['POST'])
+def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        print(username, password)
+        if username in users and users[username] == password:
+            return user(username)
+        else:
+            return '<h1>Invalid Credentials</h1>'
+    else:
+        return login_page()
+
+
 @app.route("/admin")
 def admin():
     return redirect(url_for("user", name="Admin"))
@@ -21,7 +46,12 @@ def admin():
 
 @app.route("/index")
 def index():
-    return render_template("./templates/index.html")
+    return render_template("index.html")
+
+
+@app.route("/test")
+def test():
+    return render_template("test.html")
 
 
 if __name__ == "__main__":
---
+++ static/app.js
@@ -0,0 +1,57 @@
+// DOM Manipulation
+
+//const title = document.querySelector('#main-heading');
+
+//title.style.color = 'red';
+
+//const listItems = document.querySelectorAll('.list-items')
+
+//for (i = 0; i < listItems.length; i++) {
+
+//    listItems[i].style.fontSize = '5rem'
+//}
+
+/*
+const ul = document.querySelector('ul');
+const li = document.createElement('li');
+
+// Adding Elements
+ul.append(li);
+
+// Modifying the text
+li.innerText = 'X-men';
+
+// Modifying Attributes & Classes
+
+
+li.classList.add('list-items');
+
+
+console.log(li.classList.contains('list-items'));
+
+// Remove elements
+
+//li.remove();
+//
+
+
+*/
+
+let ul = document.querySelector('ul');
+
+// console.log(ul.parentNode.parentNode);
+// console.log(ul.parentElement.parentElement);
+
+
+// const html = document.documentElement;
+// 
+// console.log(html.parentNode);
+// console.log(html.parentElement);
+
+// console.log(ul.childNodes);
+// console.log(ul.firstChild);
+// console.log(ul.lastChild);
+// 
+// ul.childNodes[1].style.backgroundColor = 'blue';
+
+
