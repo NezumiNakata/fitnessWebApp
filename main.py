b5bfd879a5eb7aa1b1d60204cc785fa624742acc
dgross16 <danielgross13382@gmail.com>
2024-03-29 20:38:29 -0400

    added login/register/session for users

--- main.py
+++ main.py
@@ -1,58 +1,128 @@
-from flask import Flask, redirect, url_for, render_template, request
-from markupsafe import escape
+from flask import Flask, redirect, url_for, render_template, request, session, flash
+from datetime import timedelta
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.orm import DeclarativeBase
+
+
+class Base(DeclarativeBase):
+    pass
 
 
 app = Flask(__name__)
+app.secret_key = 'secretkey'
+app.permanent_session_lifetime = timedelta(days=5)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SECRET_KEY'] = 'thisisasecretkey'
+db = SQLAlchemy(model_class=Base)
+db.init_app(app)
 
-users = {
-        'admin': '12345',
-        'drewPeacock': '12345'
-        }
+
+# Initializes the database
+class users(db.Model):
+    _id = db.Column("id", db.Integer, primary_key=True)
+    username = db.Column(db.String(20), nullable=False)
+    password = db.Column(db.String(80), nullable=False)
+    email = db.Column(db.String(80), nullable=False)
+
+    def __init__(self, username, password, email):
+        self.username = username
+        self.password = password
+        self.email = email
 
 
 @app.route("/")
 def home():
-    return "Hello! This the main page <h1>HELLO<h/1>"
+    user = ""
+    if "user" in session:
+        user = session["user"]
+    return f"Hello! This the main page <h1>HELLO {user}</h1>"
 
 
-@app.route("/<name>")
-def user(name):
-    return f"Hello {escape(name)}!"
+@app.route("/user", methods=['POST', 'GET'])
+def user():
+    email = None
+    if "user" in session:
+        username = session["user"]
+
+        if request.method == 'POST':
+            email = request.form["email"]
+            session["email"] = email
+            found_user = users.query.filter(users.username == username).first()
+            found_user.email = email
+            db.session.commit()
+            flash("Email was saved!", "info")
+        else:
+            if "email" in session:
+                email = session['email']
+
+        return render_template("user.html", email=email)
+    else:
+        flash("You are not logged in!")
+        return redirect(url_for("login"))
 
 
-@app.route("/login_page")
-def login_page():
-    return render_template("login.html")
+@app.route("/register", methods=['POST', 'GET'])
+def register():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        email = request.form['email']
+        found_user = users.query.filter(users.username == username).first()
+        found_email = users.query.filter(users.email == email).first()
+        if found_user:
+            flash("Username already taken", "error")
+            return redirect(url_for("register"))
+        elif found_email:
+            flash("Email already registered with an account. Try logging in.")
+            return redirect(url_for("register"))
+        else:
+            new_user = users(username, password, email)
+            db.session.add(new_user)
+            db.session.commit()
+            session.permanent = True
+            session["user"] = username
+            session["email"] = email
+            flash("Registered!", "info")
+            return redirect(url_for("user"))
+    else:
+        return render_template("register.html")
 
 
-@app.route("/login", methods=['POST'])
+@app.route("/login", methods=['POST', 'GET'])
 def login():
     if request.method == 'POST':
         username = request.form['username']
         password = request.form['password']
         print(username, password)
-        if username in users and users[username] == password:
-            return user(username)
+        found_user = users.query.filter(users.username == username).first()
+        if found_user:
+            found_password = found_user.password
+            if found_password == password:
+                session.permanent = True
+                session["user"] = found_user.username
+                session["email"] = found_user.email
+                flash("Login Successful!", "info")
+                return redirect(url_for("user"))
+            else:
+                flash("Wrong password", "error")
+                return render_template("login.html")
         else:
-            return '<h1>Invalid Credentials</h1>'
+            flash("Wrong username", "error")
+            return render_template("login.html")
     else:
-        return login_page()
+        return render_template("login.html")
 
 
-@app.route("/admin")
-def admin():
-    return redirect(url_for("user", name="Admin"))
-
-
-@app.route("/index")
-def index():
-    return render_template("index.html")
-
-
-@app.route("/test")
-def test():
-    return render_template("test.html")
+@app.route("/logout")
+def logout():
+    session.pop("user", None)
+    session.pop("email", None)
+    flash("You have been logged out!", "info")
+    return redirect(url_for("login"))
 
 
 if __name__ == "__main__":
+    with app.app_context():
+        db.create_all()
     app.run(debug=True)
